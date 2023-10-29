fe485cd3d8355108301e9afeb4b3e7c650d563b0
Logan Ravinuthala <loganravin4@gmail.com>
2023-10-29 01:18:27 -0400

    Edited homepage to match better with figma design

--- musicproject/src/App.js
+++ musicproject/src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Sound Scout</h1>
-        <p>
-        The purpose of *insert title* is for people like you to find new lesser known artists to listen to. 
+        <p className="textbox">
+        The purpose of Sound Scout is for people like you to find new lesser known artists to listen to. 
         All you have to do is enter any artist you want in the text box on the next page, and then we will give you 5 artists 
         that have a similar sound to this artist but are lesser known. 
         This way, you can find fresh artists to listen to and it’ll give recognition to some lesser known artists that deserve 
@@ -16,6 +16,7 @@ function App() {
         <div className="button">
           <a href="search-prompt.js" className="get-started-button">Get Started!</a>
         </div>
+        <p className="authors">Created by: Logan Ravinuthala, Austin Hwang, Ansh Juvvadi, Waasif Mahood</p>
       </header>
     </div>
   );
