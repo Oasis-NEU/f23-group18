93257c0d51cb9c7aa6d2bc6d213fe4f3824a8c30
Logan Ravinuthala <loganravin4@gmail.com>
2023-11-19 13:56:57 -0500

    progress made on results page

--- musicproject/src/components/RerollButton.js
+++ musicproject/src/components/RerollButton.js
@@ -3,7 +3,7 @@ import React from "react";
 function RerollButton() {
     return (
       <div className="button">
-            <a href="results" className="get-started-button">Re-Roll!</a>
+            <a href="results" className="reroll-button">Re-Roll!</a>
       </div>
     )
 }
