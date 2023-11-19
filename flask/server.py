6c53eef7f6021f1a4ff88ed110abf9f1b438f86d
Austin Hwang <austinhwang18@gmail.com>
2023-11-19 13:57:14 -0500

    search input works

--- flask/server.py
+++ flask/server.py
@@ -1,18 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from flask_cors import CORS
 import recommendation_engine
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route("/artists")
+@app.route("/artists", methods=['POST','GET'])
 def songs():
-    five_recommended_songs = recommendation_engine.recommend_call("NewJeans", 80)
-    five_recommended_songs_image_urls = [recommendation_engine.get_artist_image_url(x) for x in five_recommended_songs]
-    return {
-        "artists": five_recommended_songs, 
-        "artist_image_urls":five_recommended_songs_image_urls
-        }
+    if request.method == 'GET':
+        search_json = request.json  
+        print(search_json)
+        search_input = search_json['searchText']
+        print(search_input)
+        five_recommended_songs = recommendation_engine.recommend_call(search_input, 80)
+        five_recommended_songs_image_urls = [recommendation_engine.get_artist_image_url(x) for x in five_recommended_songs]
+        artist_data = {
+            "artists": five_recommended_songs, 
+            "artist_image_urls":five_recommended_songs_image_urls
+            }
+        return jsonify(artist_data)
+    elif request.method == 'POST':
+        search_json = request.json  
+        print(search_json)
+        search_input = search_json['searchText']
+        print(search_input)
+        five_recommended_songs = recommendation_engine.recommend_call(search_input, 80)
+        five_recommended_songs_image_urls = [recommendation_engine.get_artist_image_url(x) for x in five_recommended_songs]
+        artist_data = {
+            "artists": five_recommended_songs, 
+            "artist_image_urls":five_recommended_songs_image_urls
+            }
+        return jsonify(artist_data)
+
     
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
