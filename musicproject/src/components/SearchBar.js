c4867a01a34271f204b7c27be13e4b147a22a565
Austin Hwang <austinhwang18@gmail.com>
2023-11-19 12:31:28 -0500

    Update SearchBar.js

--- musicproject/src/components/SearchBar.js
+++ musicproject/src/components/SearchBar.js
@@ -1,33 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBar() {
+  const [searchText, setSearchText] = useState("");
 
-    
-    const [searchQuery, setSearchQuery] = useState('');
-    
-    const handleSearch = async (e) => {
-        e.preventDefault();
-    
-        // Perform the HTTP request to your backend
-        try {
-        const response = await fetch(`your-backend-url?query=${searchQuery}`);
-        const data = await response.json();
-    
-        // Process the response data as needed
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Make a POST request to your Flask backend
+    fetch("http://127.0.0.1:5000/artists", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ searchText }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the server
         console.log(data);
-        } catch (error) {
-        console.error('Error fetching data:', error);
-        }
-    };
-    
-    return (
-        <div className="search-bar">
-            <input type="text" placeholder="Enter an artist"/>
-            <i className="fa-solid fa-magnifying-glass"></i>
-        </div>
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-        
-    )
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Enter an artist"
+        value={searchText}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
+    </div>
+  );
 }
 
 export default SearchBar;
