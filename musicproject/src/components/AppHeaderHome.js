e9e03a1ea856d2135170b88bd5c40612af5c3d27
RadiantBunny633 <waasifmahmood2004@gmail.com>
2023-11-30 12:43:19 -0500

    #6: added the photo dont really wanna make another ticket for this

--- musicproject/src/components/AppHeaderHome.js
+++ musicproject/src/components/AppHeaderHome.js
@@ -6,7 +6,15 @@ import Authors from "./Authors";
 function AppHeaderHome() {
     return (
       <header className="App-header">
-        <h1>Sound Scout</h1>
+        <h1>Sound Scout 
+          <img src="/logo.png" alt="Logo" style={{
+          width: '130px', 
+          height: 'auto',
+          position: 'absolute',
+          top: '50%',
+          transform: 'translateY(-50%)',
+          marginLeft: '20px',
+        }}/></h1>
         <Description />
         <Button />
         <Authors />
