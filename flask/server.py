bf07bcfa808ce3ac202de6acd150f9fea5c6dfa9
Austin Hwang <austinhwang18@gmail.com>
2023-11-30 14:58:09 -0500

    edited code and popularity brushup

--- flask/server.py
+++ flask/server.py
@@ -14,11 +14,11 @@ def songs():
     print(search_json)
     search_input = search_json['searchText']
     print(search_input)
-    five_recommended_songs = recommendation_engine.recommend_call(search_input, 80)
-    five_recommended_songs_image_urls = [recommendation_engine.get_artist_image_url(x) for x in five_recommended_songs]
+    five_recommended_artists = recommendation_engine.recommend_call(search_input, (recommendation_engine.get_artist_popularity(search_input) - 20))
+    five_recommended_artists_image_urls = [recommendation_engine.get_artist_image_url(artist) for artist in five_recommended_artists]
     artist_data = {
-        "artists": five_recommended_songs, 
-        "artist_image_urls":five_recommended_songs_image_urls
+        "artists": five_recommended_artists, 
+        "artist_image_urls":five_recommended_artists_image_urls
         }
     print(artist_data["artists"])
     return jsonify(artist_data)
